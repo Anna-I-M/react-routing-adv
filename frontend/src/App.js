f52ef2170ff909669aa481cdbc20379ff0e78308
Anna Ilieva <ilieva.anna.a@gmail.com>
2023-04-28 21:11:43 +0200

    Added loader() to fetch data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import EditEventPage from "./pages/EditEventPage";
 import EventDetailPage from "./pages/EventDetailPage";
-import EventsPage from "./pages/EventsPage";
+import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
 import HomePage from "./pages/HomePage";
 import NewEventPage from "./pages/NewEventPage";
 import RootLayout from "./pages/Root";
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
+          { index: true, element: <EventsPage />, loader: eventsLoader },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,24 +1,26 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const EVENTS = [
-    { id: "ev1", title: "First Event" },
-    { id: "ev2", title: "Second Event" },
-    { id: "ev3", title: "Third Event" },
-  ];
+  const data = useLoaderData();
+  const events = data.events;
 
   return (
     <>
-      <h1>My Events Page</h1>
-      <ul>
-        {EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={`/events/${event.id}`}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    //...
+  } else {
+    return response;
+  }
+};
